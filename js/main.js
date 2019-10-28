8529bf56d7c903fc238bb160ba8a5c8b0a01badf
srikanth kanjarla <srikanthkanjarla@gmail.com>
2019-10-28 09:03:48 +0530

    Update Home page and cleanup code base

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// navbar background change on scroll
+const navbar = document.querySelector(".navbar");
+const videoContainer = document.querySelector(".video-container");
+
+const sectionOneOptions = {
+  rootMargin: "-80px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function( entries ) {
+  entries.forEach(entry => {
+      console.log(entry);
+    if (!entry.isIntersecting) {
+        navbar.classList.remove("navbar-light")
+      navbar.classList.add("navbar-dark");
+    } else {
+      navbar.classList.remove("navbar-dark");
+      navbar.classList.add("navbar-light");
+    }
+  });
+},
+sectionOneOptions);
+sectionOneObserver.observe(videoContainer);
+// END - Navbar background change on scroll
---
+++ js/plugins.js
@@ -0,0 +1,87 @@
+window.onload = function(){
+  // Initiate animate on scroll library
+  AOS.init();
+
+  // initiate stats
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+  // our clients/partners
+  $(".clients-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
+    }
+  });
+
+  // client testimonials
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 2
+      }
+    }
+  });
+
+  // our projects
+  $('.projects-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: false,
+    nav: true,
+    mouseDrag: false,
+    autoplay: true,
+    animateOut: 'slideOutUp',
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+
+}
+
+
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+  var method;
+  var noop = function () {};
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+}());
+
+// Place any jQuery/helper plugins in here.
