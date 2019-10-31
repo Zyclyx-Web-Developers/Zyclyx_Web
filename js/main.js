195bfe96e44fc9539905a0269455d0c99191bfa8
srikanth kanjarla <srikanthkanjarla@gmail.com>
2019-10-31 09:05:11 +0530

    Add scroll progress bar on top

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+var h = document.documentElement,
+  b = document.body,
+  st = 'scrollTop',
+  sh = 'scrollHeight',
+  progress = document.querySelector('.progress'),
+  scroll;
+
+document.addEventListener('scroll', function() {
+  scroll = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+  progress.style.setProperty('--scroll', scroll + '%');
+});
+
 // navbar background change on scroll
 const navbar = document.querySelector(".navbar");
 const videoContainer = document.querySelector(".video-container");
