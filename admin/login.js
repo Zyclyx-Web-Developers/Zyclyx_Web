ef76919d758e67c4aa67fee56f843553ee463821
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-11-15 17:00:01 +0530

    Add user login feature

--- admin/login.js
+++ admin/login.js
@@ -1,67 +1,53 @@
+(function () {
+    let form = document.getElementById("loginForm");
+    
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        form.classList.add('was-validated');
 
-let loginform=document.getElementById("LoginForm");
-var spinner=document.getElementById("spinner");
-//window.localStorage.removeItem('token');
-
-
-loginform.addEventListener("submit",function(event){
-    event.preventDefault();
-    let Email=document.getElementById("mail").value;
-    let pswd=document.getElementById("pswd").value;
-    let credential={
-        identifier:Email,
-        password:pswd
-    }
-    if(Email !==''&& pswd !==''){
-
-fetch('http://localhost:1337/auth/local',{
-method:'post',
-headers:{
-    "Content-type": "application/json"
-},
-body:JSON.stringify(credential)
-})
-.then(function(responce){
-return responce.json();
-})
-.then(function(jsondata){  
-   localStorage.setItem("token",jsondata.jwt);
-   checkcredentials();
-  
-})
-.catch(function(error){
-   //console.table("Error :",error);
-  document.getElementById("error").textContent ="Opps! something Went Wrong, Try again";
-})
-}
-else{
-   document.getElementById("error").textContent ="Invalid username or password";
-   }
-  })
-
-function checkcredentials(){
-spinner.classList.add("spinner-border");
-let dissablebtn=document.getElementById("submitBtn");
-dissablebtn.setAttribute("disabled",true);
-
-let timer=setTimeout(function(){
-spinner.classList.remove("spinner-border");
-let token=localStorage.getItem("token");
-
-if(token !== "undefined"){
- alert("Login Successfully");
+        if (form.checkValidity()) {             
+            let formData = new FormData(form); 
+            data ={ 
+                identifier:formData.get("identifier"),
+                password:formData.get("password")
+            }
  
- spinner.classList.add("spinner-border");
-  GoToDashboard();  
-}
-else{
-    document.getElementById("error").textContent ="Invalid username or password";
-}
-},3000) 
-} 
-function GoToDashboard(){
-    let timeId=setTimeout(function(){        
-   spinner.classList.remove("spinner-border");
-    window.location="Dashboard.html";
-    },1000)
-}
+            // fetch('http://localhost:1337/auth/local', {
+                fetch('https://agile-plateau-09650.herokuapp.com/auth/local', {     
+                method: 'post',
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body:JSON.stringify(data)
+            })
+                .then(function (responce) {
+                    document.getElementById("loginButton").setAttribute("disabled",true);
+                    document.getElementById("btnText").style.display = "none";
+                    document.getElementById("btnSpinner").style.display = "block";                    
+                    return responce.json();
+                })                 
+                .then(function (jsondata) {
+                   // localStorage.setItem("token", jsondata.jwt);
+                   if(jsondata.statusCode === 400){
+                    form.classList.remove('was-validated');                                          
+                    document.getElementById("errorAlert").textContent = "Username or Password Invalid";
+                    document.getElementById("loginButton").removeAttribute("disabled");
+                    document.getElementById("btnText").style.display = "block";
+                    document.getElementById("btnSpinner").style.display = "none"; 
+                    return false;
+                   }
+                    console.log(jsondata);
+                    sessionStorage.setItem("token",jsondata.jwt);
+                    sessionStorage.setItem("user",jsondata.user.username);
+                    console.log(sessionStorage.getItem("token"));
+                    if(sessionStorage.getItem("token")){
+                        window.location="dashboard.html";
+                    }
+                })
+                .catch(function (error) {
+                    document.getElementById("errorAlert").textContent = "Something went wrong! Please try again"
+                })
+         }
+    }, false)
+})();
+ 
