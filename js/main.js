a9209d8ee2432f9e3f3bbbdcf876d63b7ce8c38e
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-11-19 18:35:48 +0530

    Update Home page layout and fonts

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ document.addEventListener('scroll', function() {
 
 // NAVBAR BACKGROUND CHANGE ON SCROLL
 const navbar = document.querySelector(".navbar");
-const videoContainer = document.querySelector("#idhomeContainer");
+const homeContainer = document.querySelector("#homeContainer");
 
 const sectionOneOptions = {
   rootMargin: "-80px 0px 0px 0px"
@@ -36,17 +36,17 @@ sectionOneObserver.observe(homeContainer);
 // END - NAVBAR BACKGROUND COLOR TOGGLE ON SCROLL
 
 // DISABLE COPY PASTE AND RIGHT CLICK
-$(document).ready(function () {
-  //Disable cut copy paste
-  $('body').bind('cut copy paste', function (e) {
-      e.preventDefault();
-  });
+//Disable cut copy paste
+//Disable mouse right click
+// $(document).ready(function () {  
+//   $('body').bind('cut copy paste', function (e) {
+//       e.preventDefault();
+//   });
  
-  //Disable mouse right click
-  $("body").on("contextmenu",function(e){
-      return false;
-  });
-});
+//   $("body").on("contextmenu",function(e){
+//       return false;
+//   });
+// });
 // END - DISABLE COPY PASTE AND RIGHT CLICK
 
 // FOOTER - COPY RIGHT YEAR
