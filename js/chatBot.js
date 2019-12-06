a148f87cf44b7ca1ed68df77905a5d96c7f5ff18
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-12-06 16:53:36 +0530

    Update API path

--- js/chatBot.js
+++ js/chatBot.js
@@ -21,8 +21,8 @@ $(window).on('load', function () {
         event.preventDefault();
         let userText = document.getElementById('userText').value;
         messagesElement.innerHTML += `<p id="userReplay">${userText}</p>`
-        fetch('https://ancient-anchorage-09006.herokuapp.com/zybot', {
-            //fetch('http://localhost:3000/zybot', {             
+        //fetch('https://ancient-anchorage-09006.herokuapp.com/zybot', {
+            fetch('http://localhost:3000/zybot', {             
             method: 'post',
             headers: {
                 "Content-type": "application/json"
