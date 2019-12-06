dedf0d4a821a17bf9c6d897dfed7ad5dd27b9353
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-12-06 16:52:56 +0530

    Integrate chat bot

--- js/chatBot.js
+++ js/chatBot.js
@@ -9,8 +9,57 @@ function show(x) {
 }
 
 
+
 // Page Loading Indicator
 $(window).on('load', function () {
     // Animate loader off screen
     $(".se-pre-con").fadeOut("slow");
-  })
+
+    // Chat bot 
+    let messagesElement = document.getElementById("botMessages");
+    document.getElementById('bot-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        let userText = document.getElementById('userText').value;
+        messagesElement.innerHTML += `<p id="userReplay">${userText}</p>`
+        fetch('https://ancient-anchorage-09006.herokuapp.com/zybot', {
+            //fetch('http://localhost:3000/zybot', {             
+            method: 'post',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({ text: userText })
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (output) {   
+                console.log(output)              
+                document.getElementById('userText').value = '';
+
+                if (output.response[0].text) {
+                    messagesElement.innerHTML += `<p id="botReplay">${output.response[0].text}</p>`
+                }
+
+                if (output.response[0].title) {
+                    messagesElement.innerHTML += `<p id="botReplay">${output.response[0].title}</p>`;
+                }
+
+                if (output.response[0].description) {
+                    messagesElement.innerHTML += `<p id="botReplay">${output.response[0].description}</p>`;
+                }
+
+                if (output.response[0].options) {
+                    let html = `<p id="botReplay"> `;
+                    html += output.response[0].options.map(function (option) {
+                        return option.value.input.text;
+                    }).join(', ')
+                    html += '</p>';
+                    messagesElement.innerHTML += html;
+                }
+
+            })
+            // .catch(function (err) {
+            //     console.log(err);
+            // })
+    });
+})
