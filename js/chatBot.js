1ee025e6fc8cb057fa497ff666722faf54f9eefc
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-12-10 11:25:45 +0530

    Fix chatbot response issue for first message

--- js/chatBot.js
+++ js/chatBot.js
@@ -1,65 +1,62 @@
 function show(x) {
-    if (x == 0) {
-        document.getElementById("box1").style.display = "none"
-    }
-    else {
-
-        document.getElementById("box1").style.display = "inline"
-    }
+  if (x == 0) {
+    document.getElementById("box1").style.display = "none";
+  } else {
+    document.getElementById("box1").style.display = "inline";
+  }
 }
 
-
-
 // Page Loading Indicator
-$(window).on('load', function () {
-    // Animate loader off screen
-    $(".se-pre-con").fadeOut("slow");
+$(window).on("load", function() {
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");
 
-    // Chat bot 
-    let messagesElement = document.getElementById("botMessages");
-    document.getElementById('bot-form').addEventListener('submit', function (event) {
-        event.preventDefault();
-        let userText = document.getElementById('userText').value;
-        messagesElement.innerHTML += `<p id="userReplay">${userText}</p>`
-        fetch('https://ancient-anchorage-09006.herokuapp.com/zybot', {
-            //fetch('http://localhost:3000/zybot', {             
-            method: 'post',
-            headers: {
-                "Content-type": "application/json"
-            },
-            body: JSON.stringify({ text: userText })
+  // Chat bot
+  let messagesElement = document.getElementById("botMessages"); 
+  document
+    .getElementById("bot-form")
+    .addEventListener("submit", function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      let userText = document.getElementById("userText").value;
+      messagesElement.innerHTML += `<p id="userReplay">${userText}</p>`;
+      fetch('https://ancient-anchorage-09006.herokuapp.com/zybot', {
+      //fetch("http://localhost:3000/zybot", {
+        method: "post",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({ text: userText })
+      })
+        .then(function(response) {
+          document.getElementById("userText").value = "";
+          return response.json();
+        })     
+        .then(function(output){             
+            if(output.response){
+             if (output.response[0].text) {
+                messagesElement.innerHTML += `<p id="botReplay">${output.response[0].text}</p>`;
+              }
+    
+              if (output.response[0].title) {
+                messagesElement.innerHTML += `<p id="botReplay">${output.response[0].title}</p>`;
+              }
+    
+              if (output.response[0].description) {
+                messagesElement.innerHTML += `<p id="botReplay">${output.response[0].description}</p>`;
+              }
+    
+              if (output.response[0].options) {
+                let html = `<p id="botReplay"> `;
+                html += output.response[0].options
+                  .map(function(option) {
+                    return option.value.input.text;
+                  })
+                  .join(", ");
+                html += "</p>";
+                messagesElement.innerHTML += html;
+              }    
+            }            
         })
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (output) {   
-                console.log(output)              
-                document.getElementById('userText').value = '';
-
-                if (output.response[0].text) {
-                    messagesElement.innerHTML += `<p id="botReplay">${output.response[0].text}</p>`
-                }
-
-                if (output.response[0].title) {
-                    messagesElement.innerHTML += `<p id="botReplay">${output.response[0].title}</p>`;
-                }
-
-                if (output.response[0].description) {
-                    messagesElement.innerHTML += `<p id="botReplay">${output.response[0].description}</p>`;
-                }
-
-                if (output.response[0].options) {
-                    let html = `<p id="botReplay"> `;
-                    html += output.response[0].options.map(function (option) {
-                        return option.value.input.text;
-                    }).join(', ')
-                    html += '</p>';
-                    messagesElement.innerHTML += html;
-                }
-
-            })
-            // .catch(function (err) {
-            //     console.log(err);
-            // })
     });
-})
+});
